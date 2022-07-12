5ba16d05a832022edc5dbdc8d874b68b761f32e5
ARAVIND-KUMAR-POTHULA <76431331+ARAVIND-KUMAR-POTHULA@users.noreply.github.com>
2022-07-12 10:15:40 +0530

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,47 @@
+def getSet() {
+    script {
+          def now = LocalDateTime.now()
+          day = now.format(DateTimeFormatter.ofPattern("E"))
+          println (day)
+          if(day == 'Mon'){
+                 println ("Monday")
+                 set = 'VI_01'
+                 println(set)
+                }
+                else if(day == 'Tue'){
+                    println ("Tuesday")
+                    set = 'VI_02'
+                    println(set)
+                }
+                else if(day == 'Wed'){
+                    println ("Wednesday")
+                    set = 'VI_03'
+                    println(set)
+                }
+                else if(day == 'Thu'){
+                    println ("Thursday")
+                    set = 'AC_20'
+                    println(set)
+                }
+                else if(day == 'Fri'){
+                    println ("Friday")
+                    set = 'AC_20'
+                    println(set)
+                }
+                else if(day == 'Sat'){
+                    println ("Friday")
+                    set = 'VI_07'
+                    println(set)
+                }
+                else if(day == 'Sun'){
+                    println ("Friday")
+                    set = 'VI_07'
+                    println(set)
+                }
+                else{
+                 println ("No Environment required")   
+                }
+         }
+     
+} 
+return this
